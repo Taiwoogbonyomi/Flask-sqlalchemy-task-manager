f4fbb6bbf8337f8004d773140d449232ba73bd04
Taiwo Ogbonyomi <taiwoogbonyomi@gmail.com>
2024-10-19 23:08:20 +0000

    Add requirement.txt file and modify init.py

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -5,11 +5,16 @@ from flask_sqlalchemy import SQLAlchemy
 if os.path.exists("env.py"):
     import env  # noqa
 
+ app = Flask(__name__)
+ app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 
-app = Flask(__name__)
-app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
-
-db = SQLAlchemy(app)
+ if os.environ.get("DEVELOPMENT") == "True":
+     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+ else:
+     else:
+     uri = os.environ.get("DATABASE_URL")
+     if uri.startswith("postgres://"):
+         uri = uri.replace("postgres://", "postgresql://", 1)
+     app.config["SQLALCHEMY_DATABASE_URI"] = uri
 
 from taskmanager import routes  # noqa
